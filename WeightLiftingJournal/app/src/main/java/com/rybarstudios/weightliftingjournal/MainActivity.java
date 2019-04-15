3b2d31c7e92003a918deef94bb9db92e0b4a8fd0
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-15 11:08:19 -0400

    Initial commit

---
+++ WeightLiftingJournal/app/src/main/java/com/rybarstudios/weightliftingjournal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rybarstudios.weightliftingjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
