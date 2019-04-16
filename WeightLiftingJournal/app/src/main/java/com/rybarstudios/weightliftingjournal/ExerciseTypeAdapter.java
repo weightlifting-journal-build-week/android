c74cca9e3373c26f4c7445cc07bd7ef288ce7523
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-15 22:05:53 -0400

    Updated network adapter to include POST functionality

--- WeightLiftingJournal/app/src/main/java/com/rybarstudios/weightliftingjournal/ExerciseTypeAdapter.java
+++ WeightLiftingJournal/app/src/main/java/com/rybarstudios/weightliftingjournal/ExerciseTypeAdapter.java
@@ -1,17 +1,22 @@
 package com.rybarstudios.weightliftingjournal;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ExerciseTypeAdapter extends RecyclerView.Adapter<ExerciseTypeAdapter.ExerciseTypeViewHolder> {
-
+    Context context;
 
     @NonNull
     @Override
-    public ExerciseTypeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ExerciseTypeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.exercise_type_item_list, parent, false);
+        return new ExerciseTypeViewHolder(view);
     }
 
     @Override
@@ -25,9 +30,13 @@ public class ExerciseTypeAdapter extends RecyclerView.Adapter<ExerciseTypeAdapte
     }
 
     class ExerciseTypeViewHolder extends RecyclerView.ViewHolder {
+        View exerciseTypeParentLayout;
+        TextView exerciseTypeTextView;
 
         public ExerciseTypeViewHolder(@NonNull View itemView) {
             super(itemView);
+            exerciseTypeParentLayout = itemView.findViewById(R.id.exercise_type_parent_layout);
+            exerciseTypeTextView = itemView.findViewById(R.id.exercise_type_text_view);
         }
     }
 }
--- WeightLiftingJournal/app/src/main/java/com/rybarstudios/weightliftingjournal/NetworkAdapter.java
+++ WeightLiftingJournal/app/src/main/java/com/rybarstudios/weightliftingjournal/NetworkAdapter.java
@@ -3,47 +3,83 @@ package com.rybarstudios.weightliftingjournal;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 import java.util.stream.Stream;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class NetworkAdapter {
     public static final int CONNECT_TIMEOUT = 3000;
     public static final int READ_TIMEOUT = 3000;
 
-    public static String httpGetRequest(final String urlString) {
-        String result = "";
-        HttpURLConnection connection = null;
-        InputStream stream = null;
+    public static final String GET     = "GET";
+    public static final String POST    = "POST";
+    public static final String HEAD    = "HEAD";
+    public static final String OPTIONS = "OPTIONS";
+    public static final String PUT     = "PUT";
+    public static final String DELETE  = "DELETE";
+    public static final String TRACE   = "TRACE";
+
+    static String httpRequest(String urlString) {
+        return httpRequest(urlString, GET, null, null);
+    }
+
+    static String httpRequest(String urlString, String requestMethod) {
+        return httpRequest(urlString, requestMethod, null, null);
+    }
+
+    static String httpRequest(String urlString, String requestMethod, JSONObject requestBody, Map<String, String> headerProperties) {
+        String             result      = "";
+        InputStream        inputStream = null;
+        HttpsURLConnection connection  = null;
+
         try {
             URL url = new URL(urlString);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setConnectTimeout(CONNECT_TIMEOUT);
+            connection = (HttpsURLConnection) url.openConnection();
             connection.setReadTimeout(READ_TIMEOUT);
-            connection.connect();
+            connection.setConnectTimeout(CONNECT_TIMEOUT);
 
-            int responseCode = connection.getResponseCode();
-            if (responseCode != HttpURLConnection.HTTP_OK) {
-                result = Integer.toString(responseCode);
-                throw new IOException("HTTP error code: " + responseCode);
+            connection.setRequestMethod(requestMethod);
+
+            if (headerProperties != null) {
+                for (Map.Entry<String, String> property : headerProperties.entrySet()) {
+                    connection.setRequestProperty(property.getKey(), property.getValue());
+                }
+            }
+            if((requestMethod.equals(POST) || requestMethod.equals(PUT)) && requestBody != null) {
+                connection.setDoInput(true);
+                final OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(requestBody.toString().getBytes());
+                outputStream.close();
+            } else {
+                connection.connect();
             }
 
-            stream = connection.getInputStream();
-            if (stream != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuilder builder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                    line = reader.readLine();
+            final int responseCode = connection.getResponseCode();
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                inputStream = connection.getInputStream();
+                if (inputStream != null) {
+                    BufferedReader reader  = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder  builder = new StringBuilder();
+
+                    String line;
+                    do {
+                        line = reader.readLine();
+                        builder.append(line);
+                    } while (line != null);
+                    result = builder.toString();
                 }
-                result = builder.toString();
             }
 
         } catch (MalformedURLException e) {
@@ -51,6 +87,14 @@ public class NetworkAdapter {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
             if (connection != null) {
                 connection.disconnect();
             }
